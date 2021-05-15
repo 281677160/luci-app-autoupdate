8638a525f7aa2ebfe19a61c631d992366cf0982f
281677160 <66651997+281677160@users.noreply.github.com>
2021-05-16 02:01:31 +0800

    Update Check_Update.sh

--- root/usr/share/autoupdate/Check_Update.sh
+++ root/usr/share/autoupdate/Check_Update.sh
@@ -7,17 +7,16 @@ if [ ! -f /bin/AutoUpdate.sh ];then
 	echo "未检测到 /bin/AutoUpdate.sh" > /tmp/cloud_version
 	exit
 fi
-CURRENT_DEVICE="$(awk 'NR==3' /etc/openwrt_info)"
-Firmware_COMP1="$(awk 'NR==5' /etc/openwrt_info)"
-Firmware_COMP2="$(awk 'NR==6' /etc/openwrt_info)"
-[[ -z "${CURRENT_DEVICE}" ]] && CURRENT_DEVICE="$(jsonfilter -e '@.model.id' < "/etc/board.json" | tr ',' '_')"
-Github="$(awk 'NR==2' /etc/openwrt_info)"
+[ -f /etc/openwrt_info ] && source /etc/openwrt_info || {
+	echo -e "\n未检测到 /etc/openwrt/info,无法运行更新程序!"
+	exit
+}
+[[ -z "${DEFAULT_Device}" ]] && DEFAULT_Device="$(jsonfilter -e '@.model.id' < "/etc/board.json" | tr ',' '_')"
 [[ -z "${Github}" ]] && exit
 Author="${Github##*com/}"
 Github_Tags="https://api.github.com/repos/${Author}/releases/tags/update_Firmware"
 wget -q ${Github_Tags} -O - > /tmp/Github_Tags
-Firmware_Type="$(awk 'NR==4' /etc/openwrt_info)"
-case ${CURRENT_DEVICE} in
+case ${DEFAULT_Device} in
 x86-64)
 	if [ -d /sys/firmware/efi ];then
 		Firmware_SFX="-UEFI.${Firmware_Type}"
@@ -32,9 +31,8 @@ x86-64)
 	BOOT_Type=""
 ;;
 esac
-Cloud_Ver="$(cat /tmp/Github_Tags | egrep -o "${Firmware_COMP1}-${Firmware_COMP2}-${CURRENT_DEVICE}-[0-9]+.*?[0-9]+${Firmware_SFX}" | awk 'END {print}' | egrep -o '[a-zA-Z0-9_-]+.*?[0-9]+')"
+Cloud_Ver="$(cat /tmp/Github_Tags | egrep -o "${Firmware_COMP1}-${Firmware_COMP2}-${DEFAULT_Device}-[0-9]+.*?[0-9]+${Firmware_SFX}" | awk 'END {print}' | egrep -o '[a-zA-Z0-9_-]+.*?[0-9]+')"
 Cloud_Version="${Cloud_Ver#*${Firmware_COMP1}-}"
-CURRENT_Version="$(awk 'NR==1' /etc/openwrt_info)"
 if [[ ! -z "${Cloud_Version}" ]];then
 	if [[ "${CURRENT_Version}" -eq "${Cloud_Version}" ]];then
 		Checked_Type="已是最新"
