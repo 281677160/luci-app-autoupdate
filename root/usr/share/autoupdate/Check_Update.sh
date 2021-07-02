45c733d09ccf4329d6c586a0e3cadc460127e2d8
281677160 <66651997+281677160@users.noreply.github.com>
2021-07-02 15:27:56 +0800

    Update Check_Update.sh

--- root/usr/share/autoupdate/Check_Update.sh
+++ root/usr/share/autoupdate/Check_Update.sh
@@ -2,56 +2,32 @@
 # https://github.com/Hyy2001X/AutoBuild-Actions
 # AutoBuild Module by Hyy2001
 
+export Google_Check=$(curl -I -s --connect-timeout 8 google.com -w %{http_code} | tail -n1)
+if [ ! "$Google_Check" == 301 ];then
+	echo "网络检测失败,因Github现在也筑墙了,请先使用梯子翻墙再来尝试!" > /tmp/cloud_version
+	sleep 2
+	exit 1
+fi
 rm -f /tmp/cloud_version
 if [ ! -f /bin/AutoUpdate.sh ];then
 	echo "未检测到定时更新插件程序" > /tmp/cloud_version
 	exit 1
+else
+	bash /bin/AutoUpdate.sh	-w
 fi
-[ -f /etc/openwrt_info ] && chmod +x /etc/openwrt_info
-[ -f /etc/openwrt_info ] && source /etc/openwrt_info || {
-	echo "未检测到定时更新插件程序!" > /tmp/cloud_version
-	exit 1
-}
-Apidz="${Github##*com/}"
-Github_Tags=https://api.github.com/repos/${Apidz}/releases/tags/AutoUpdate
-export Google_Check=$(curl -I -s --connect-timeout 8 google.com -w %{http_code} | tail -n1)
-if [ ! "$Google_Check" == 301 ];then
-	echo "网络检测失败，Github已筑墙，请翻墙或者您的是私有仓库!" > /tmp/cloud_version
-	exit 1
-fi
-[[ -z ${CURRENT_Version} ]] && echo "本地固件版本获取失败,请检查/etc/openwrt_info文件的值!" > /tmp/cloud_version && exit 1
-[[ -z "${Github}" ]]  && echo "Github地址获取失败,请检查/etc/openwrt_info文件的值!" > /tmp/cloud_version && exit 1
-[ ! -d ${Download_Path} ] && mkdir -p ${Download_Path}
-wget -q --timeout 5 ${Github_Tags} -O ${Download_Path}/Github_Tags
-case ${DEFAULT_Device} in
-x86-64)
-	if [ -d /sys/firmware/efi ];then
-		Firmware_SFX="-UEFI.${Firmware_Type}"
-		BOOT_Type="-UEFI"
-	else
-		Firmware_SFX="-Legacy.${Firmware_Type}"
-		BOOT_Type="-Legacy"
-	fi
-;;
-*)
-	Firmware_SFX=".${Firmware_Type}"
-	BOOT_Type="Sysupg"
-;;
-esac
-export CLOUD_Firmware="$(egrep -o "${Egrep_Firmware}-[0-9]+${Firmware_SFX}" ${Download_Path}/Github_Tags | awk 'END {print}')"
-export CLOUD_Version="$(echo ${CLOUD_Firmware} | egrep -o "${REPO_Name}-${DEFAULT_Device}-[0-9]+")"
+source /tmp/Version_Tags
 if [[ ! -z "${CLOUD_Version}" ]];then
 	if [[ "${CURRENT_Version}" -eq "${CLOUD_Version}" ]];then
 		Checked_Type="已是最新"
-		echo "${CLOUD_Version}${BOOT_Type} [${Checked_Type}]" > /tmp/cloud_version
+		echo "${CLOUD_Version} [${Checked_Type}]" > /tmp/cloud_version
 	elif [[ "${CURRENT_Version}" -gt "${CLOUD_Version}" ]];then
 		Checked_Type="发现更新"
-		echo "${CLOUD_Version}${BOOT_Type} [${Checked_Type}]" > /tmp/cloud_version
+		echo "${CLOUD_Version} [${Checked_Type}]" > /tmp/cloud_version
 	elif [[ "${CURRENT_Version}" -lt "${CLOUD_Version}" ]];then
 		Checked_Type="当前的版本高于云端现有版本"
-		echo "${CLOUD_Version}${BOOT_Type} [${Checked_Type}]" > /tmp/cloud_version	
+		echo "${CLOUD_Version} [${Checked_Type}]" > /tmp/cloud_version	
 	fi
 else
-	echo "没检测到云端固件版本，您可能把云端固件删除了，或云端版本和此固件名称格式不对称，如果名称格式不对称请把云端固件全部删除，然后重新编译固件!" > /tmp/cloud_version
+	echo "没检测到云端固件，您可能把云端固件删除了，或格式不对称，比如爱快虚拟机安装固件不管什么格式都会变成Legacy引导!" > /tmp/cloud_version
 fi
 exit 0
