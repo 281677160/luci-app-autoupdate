43634e1cb574f07fa4f646344f1e514e431ae745
281677160 <66651997+281677160@users.noreply.github.com>
2022-03-26 21:26:26 +0800

    Update Check_Update.sh

--- root/usr/share/autoupdate/Check_Update.sh
+++ root/usr/share/autoupdate/Check_Update.sh
@@ -2,26 +2,39 @@
 # https://github.com/Hyy2001X/AutoBuild-Actions
 # AutoBuild Module by Hyy2001
 
-rm -f /tmp/cloud_version
-rm -f /tmp/Version_Tags
+
+[[ -f /tmp/cloud_version ]] && rm -f /tmp/cloud_version
+[[ -f /tmp/Version_Tags ]] && rm -f /tmp/Version_Tags
+[[ -f /tmp/baidu.html ]] && rm -rf /tmp/baidu.html
+
+wget -q -p /tmp http://www.baidu.com/ -O /tmp/baidu.html
+if [[ -f /tmp/baidu.html ]] && [[ `grep -c "百度一下" /tmp/baidu.html` -ge '1' ]];then
+	echo "OK"
+	rm -rf /tmp/baidu.html
+else
+	echo "您可能没进行联网,请检查网络!" > /tmp/cloud_version
+	exit 1
+fi
+
 if [[ -f /bin/openwrt_info ]]; then
 	chmod +x /bin/openwrt_info
-        source /bin/openwrt_info
+	source /bin/openwrt_info
 	if [[ $? -ne 0 ]];then
-          echo "openwrt_info数据有误运行失败,请检查openwrt_info文件!" > /tmp/cloud_version
-          exit 1
+		echo "openwrt_info数据有误运行失败,请检查openwrt_info文件!" > /tmp/cloud_version
+		exit 1
         fi
 	chmod +x /bin/AutoUpdate.sh
 	bash /bin/AutoUpdate.sh	-w
         if [[ $? -ne 0 ]];then
-          echo "AutoUpdate.sh数据有误运行失败,请检查AutoUpdate.sh文件!" > /tmp/cloud_version
-          exit 1
+		echo "AutoUpdate.sh数据有误运行失败,请检查AutoUpdate.sh文件!" > /tmp/cloud_version
+		exit 1
         fi
 else
 	echo "未检测到openwrt_info文件,无法运行更新程序!" > /tmp/cloud_version
 	exit 1
 fi
-[[ ! -f /tmp/Version_Tags ]] && echo "未检测到云端版本,请检查网络,或您的仓库为私库,或您修改的Github地址有错误,或发布已被删除,或再次刷新网页试试!" > /tmp/cloud_version && exit 1
+
+[[ ! -f /tmp/Version_Tags ]] && echo "未检测到云端版本,或您的仓库为私库,或您修改的Github地址有错误,或发布已被删除!" > /tmp/cloud_version && exit 1
 chmod +x /tmp/Version_Tags && source /tmp/Version_Tags
 if [[ -n "${CLOUD_Firmware}" ]];then
 	if [[ "${LOCAL_Firmware}" -eq "${CLOUD_Firmware}" ]];then
