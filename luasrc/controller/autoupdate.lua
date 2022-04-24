3a185350e7b82f9404a8f0229c58e684cd18e540
281677160 <66651997+281677160@users.noreply.github.com>
2022-04-24 16:34:15 +0800

    Update autoupdate.lua

--- luasrc/controller/autoupdate.lua
+++ luasrc/controller/autoupdate.lua
@@ -1,5 +1,5 @@
 module("luci.controller.autoupdate",package.seeall)
 
 function index()
-	entry({"admin","system","autoupdate"},cbi("autoupdate"),_("AutoUpdate"),90)
+	entry({"admin","system","autoupdate"},cbi("autoupdate"),_("AutoUpdate"),89)
 end
