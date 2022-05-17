662b96b5e285edcfe95c05cbe7e304d58d3c81a0
281677160 <66651997+281677160@users.noreply.github.com>
2022-05-18 03:51:39 +0800

    Update Check_Update.sh

--- root/usr/share/autoupdate/Check_Update.sh
+++ root/usr/share/autoupdate/Check_Update.sh
@@ -38,28 +38,29 @@ if [[ -f "/usr/bin/AutoUpdate" ]]; then
 	if [[ $? -ne 0 ]];then
 		echo "您现在所用的Github地址上没检测到云端存在,或您的仓库为私库!" > /tmp/cloud_version
 		exit 1
-        fi
+	fi
 else
 	echo "固件不存在AutoUpdate.sh执行文件!" > /tmp/cloud_version
 	exit 1
 fi
 
 if [[ -f "/tmp/Version_Tags" ]]; then
-	LOCAL_Firmware="$(grep 'LOCAL_Firmware=' "/tmp/Version_Tags" | cut -d "=" -f2)"
-	CLOUD_Firmware="$(grep 'CLOUD_Firmware=' "/tmp/Version_Tags" | cut -d "=" -f2)"
+	LOCAL_Firmware="$(grep 'LOCAL_Firmware=' "/tmp/Version_Tags" | cut -d "-" -f4)"
+	CLOUD_Firmware="$(grep 'CLOUD_Firmware=' "/tmp/Version_Tags" | cut -d "-" -f4)"
+	CLOUD_Firmware2="$(grep 'CLOUD_Firmware=' "/tmp/Version_Tags" | cut -d "=" -f2)"
 else
 	echo "未知原因获取不了版本信息" > /tmp/cloud_version
 	exit 1
 fi
 
 if [[ -n "${CLOUD_Firmware}" ]]; then
-	if [[ "${LOCAL_Firmware}" -eq "${CLOUD_Firmware}" ]]; then
+	if [[ "${LOCAL_Firmware}" == "${CLOUD_Firmware}" ]]; then
 		Checked_Type="已是最新"
 		echo "${CLOUD_Firmware} [${Checked_Type}]" > /tmp/cloud_version
-	elif [[ "${LOCAL_Firmware}" -gt "${CLOUD_Firmware}" ]]; then
+	elif [[ "${LOCAL_Firmware}" -lt "${CLOUD_Firmware}" ]]; then
 		Checked_Type="发现更高版本固件可更新"
 		echo "${CLOUD_Firmware} [${Checked_Type}]" > /tmp/cloud_version
-	elif [[ "${LOCAL_Firmware}" -lt "${CLOUD_Firmware}" ]]; then
+	elif [[ "${LOCAL_Firmware}" -gt "${CLOUD_Firmware}" ]]; then
 		Checked_Type="云端最高版本固件,低于您现在所安装的版本,请到云端查看原因"
 		echo "${CLOUD_Firmware} [${Checked_Type}]" > /tmp/cloud_version	
 	fi
