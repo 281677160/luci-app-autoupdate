2f4a610540a6a103f5b04cf54412f6bb9499219f
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-24 03:17:46 +0800

    Update Check_Update.sh

--- root/usr/share/autoupdate/Check_Update.sh
+++ root/usr/share/autoupdate/Check_Update.sh
@@ -5,7 +5,7 @@
 [[ -f "/tmp/Version_Tags" ]] && rm -rf /tmp/Version_Tags
 
 if [[ -f "/usr/bin/AutoUpdate" ]] && [[ -f "/bin/openwrt_info" ]]; then
-	AutoUpdate -w
+	AutoUpdate
 	if [[ $? -ne 0 ]]; then
 		exit 1
 	fi
@@ -14,25 +14,18 @@ else
 	exit 1
 fi
 
-if [[ -f "/tmp/Version_Tags" ]]; then
-	LOCAL_Firmware="$(grep 'LOCAL_Firmware=' "/tmp/Version_Tags" | cut -d "-" -f4)"
-	CLOUD_Firmware="$(grep 'CLOUD_Firmware=' "/tmp/Version_Tags" | cut -d "-" -f4)"
-	CLOUD_Firmware2="$(grep 'CLOUD_Firmware=' "/tmp/Version_Tags" | cut -d "=" -f2)"
-else
-	echo "未知原因获取不了版本信息" > /tmp/cloud_version
-	exit 1
-fi
+source /tmp/Version_Tags
 
-if [[ -n "${CLOUD_Firmware}" ]]; then
-	if [[ "${LOCAL_Firmware}" == "${CLOUD_Firmware}" ]]; then
+if [[ -n "${CLOUD_Version}" ]]; then
+	if [[ "${LOCAL_Version}" == "${CLOUD_Version}" ]]; then
 		Checked_Type="已是最新"
-		echo "${CLOUD_Firmware2} [${Checked_Type}]" > /tmp/cloud_version
-	elif [[ "${LOCAL_Firmware}" -lt "${CLOUD_Firmware}" ]]; then
+		echo "${LUCI_Firmware} [${Checked_Type}]" > /tmp/cloud_version
+	elif [[ "${LOCAL_Version}" -lt "${CLOUD_Version}" ]]; then
 		Checked_Type="发现更高版本固件可更新"
-		echo "${CLOUD_Firmware2} [${Checked_Type}]" > /tmp/cloud_version
-	elif [[ "${LOCAL_Firmware}" -gt "${CLOUD_Firmware}" ]]; then
+		echo "${LUCI_Firmware} [${Checked_Type}]" > /tmp/cloud_version
+	elif [[ "${LOCAL_Version}" -gt "${CLOUD_Version}" ]]; then
 		Checked_Type="云端最高版本固件,低于您现在所安装的版本,请到云端查看原因"
-		echo "${CLOUD_Firmware2} [${Checked_Type}]" > /tmp/cloud_version	
+		echo "${LUCI_Firmware} [${Checked_Type}]" > /tmp/cloud_version	
 	fi
 else
 	echo "未知原因获取不了云端固件的版本信息" > /tmp/cloud_version
