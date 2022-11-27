aface025d4dae4d3b8b5ae01b9189d8b6f5f1cd0
281677160 <66651997+281677160@users.noreply.github.com>
2022-11-27 17:40:05 +0800

    Update Check_Update.sh

--- root/usr/share/autoupdate/Check_Update.sh
+++ root/usr/share/autoupdate/Check_Update.sh
@@ -16,8 +16,7 @@ fi
 LOCAL_Version=$(grep LOCAL_Version= /tmp/Version_Tags | cut -c15-100)
 CLOUD_Version=$(grep CLOUD_Version= /tmp/Version_Tags | cut -c15-100)
 LUCI_Firmware=$(grep LUCI_Firmware= /tmp/Version_Tags | cut -c15-100)
-
-if [[ -n "${LOCAL_Version}" ]] && [[ -n "${CLOUD_Version}" ]]; then
+if [[ -n "${LOCAL_Version}" ]] && [[ -n "${CLOUD_Version}" ]] && [[ -n "${LUCI_Firmware}" ]]; then
 	if [[ "${LOCAL_Version}" -eq "${CLOUD_Version}" ]]; then
 		Checked_Type="已是最新"
 		echo "${LUCI_Firmware} [${Checked_Type}]" > /tmp/cloud_version
